db71c918861b1bd15729232a45dd7b833a264a7e
Pius Muriithi <piusmuriithi1@gmail.com>
2021-03-06 10:43:31 +0300

    Implements form state and change handlers

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,12 +1,25 @@
-import React from "react";
-import { Prompt } from "react-router-dom";
+import React, { useState } from "react";
+import CourseForm from "./CourseForm";
 
 const ManageCoursePage = (props) => {
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    authorId: null,
+    category: "",
+  });
+
+  //with destructuring
+  function handleChange({ target }) {
+    setCourse({
+      ...course,
+      [target.name]: target.value,
+    });
+  }
   return (
     <>
       <h2>Manage Course</h2>
-      <Prompt when={true} message="Are you sure you want to leave?" />
-      {props.match.params.slug}
+      <CourseForm course={course} onChange={handleChange} />
     </>
   );
 };
